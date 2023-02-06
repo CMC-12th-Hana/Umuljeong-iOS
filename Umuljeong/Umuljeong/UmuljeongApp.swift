26c6566797908484daa1cb31feb9bd21adfc0540
hyeun <shippingbok@gmail.com>
2023-02-06 14:58:48 +0900

    create project

---
+++ Umuljeong/Umuljeong/UmuljeongApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmuljeongApp.swift
+//  Umuljeong
+//
+//  Created by 박혜운 on 2023/02/06.
+//
+
+import SwiftUI
+
+@main
+struct UmuljeongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
