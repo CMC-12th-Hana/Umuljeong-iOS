e54044b9bc9481f534d864b6cbe51990d72c9e0f
hyeun <shippingbok@gmail.com>
2023-02-23 15:41:41 +0900

    refactor: horizentalPaddingDefault 값 생성
    모든 앱 화면에서 사용되는 padding 값을 추후에 간단히 수정하기 위해 통합

---
+++ Umuljeong/Umuljeong/Common/DesignGuide/Padding.swift
@@ -0,0 +1,13 @@
+//
+//  Padding.swift
+//  Umuljeong
+//
+//  Created by 박혜운 on 2023/02/23.
+//
+
+import SwiftUI
+import UIKit
+
+extension CGFloat {
+    static let horizentalPaddingDefault:CGFloat = 15
+}
