16d7f6b435b5b3bd8fe7a0401593c0f637737962
hyeun <shippingbok@gmail.com>
2023-02-23 14:58:19 +0900

    docs: AddTaskView, AddTaskViewModel

---
+++ Umuljeong/Umuljeong/Presentation/View/Tab + Home/AddTaskView.swift
@@ -0,0 +1,20 @@
+//
+//  AddTaskView.swift
+//  Umuljeong
+//
+//  Created by 박혜운 on 2023/02/23.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTaskView()
+    }
+}
---
+++ Umuljeong/Umuljeong/Presentation/View/Tab + Home/ViewModel/AddTaskViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  AddTaskViewModel.swift
+//  Umuljeong
+//
+//  Created by 박혜운 on 2023/02/23.
+//
+
+import Foundation
+
